fa9ae430d7492905c7e0a1b5c68f7e22800e3c80
JanviTiwari48 <211301506+JanviTiwari48@users.noreply.github.com>
2026-09-04 10:23:17 +0530

    Initial project setup with database connection

---
+++ src/main/java/com/janvi/lifecarepathology/LifeCarePathologyApplication.java
@@ -0,0 +1,13 @@
+package com.janvi.lifecarepathology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeCarePathologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeCarePathologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/janvi/lifecarepathology/LifeCarePathologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janvi.lifecarepathology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeCarePathologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
